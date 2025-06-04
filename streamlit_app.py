0e499de97249941b2fa46384fa4013d3ce6911e1
Anjana-KTS <113975729+Anjana-KTS@users.noreply.github.com>
2025-06-04 14:14:58 +0530

    Update streamlit_app.py
    changes based on addition of a column called SEARCH_ON to your FRUIT_OPTIONS

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,9 @@ st.write('The name on your Smoothie will be: ', Name_on_Smoothie)
 cnx=st.connection("snowflake",type="snowflake")
 session=cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
-
+st.stop()
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients",
     my_dataframe,
@@ -26,6 +26,7 @@ if(ingredients_list):
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+= fruit_chosen +' '
+        st.subheader(fruit_chosen +'Nutritional Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     st.write(ingredients_string)
